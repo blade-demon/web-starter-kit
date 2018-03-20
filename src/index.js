1e00950a6af3a24644b3d87f16f1c7684aa37388
blade-demon <949659359@qq.com>
2018-03-20 20:39:28 +0800

    fix --hot error

--- src/index.js
+++ src/index.js
@@ -1,9 +1,1 @@
-import _ from 'lodash';
-
-// import ( /* webpackChunkName: "lodash" */ 'lodash').then(module => {
-    // var _ = module.default;
-    var data = _.join(['1','2'], '-');
-    console.log(data);
-// });
-
-
+import './css/style.css';
--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,7 @@ const config = {
     ],
     output: {
         filename: '[name].bundle.js',
-        // chunkFilename: '[name].bundle.js',
+        chunkFilename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
     devServer: {
