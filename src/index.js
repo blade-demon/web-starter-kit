9a5aad3faa34be7fdf71c65f6166a811add9842f
ziwei <ziwei@gamepoch.com>
2018-03-20 19:22:31 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+// import _ from 'lodash';
+// import axios from 'axios';
+
+// axios.get('https://api.github.com').then((response) => {
+//     console.log(response.data);
+// })
---
+++ webpack.config.js
@@ -0,0 +1,74 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
+
+
+const VENDOR_LIBS = ['lodash', 'axios'];
+
+const config = {
+    entry: {
+        app: './src/index.js',
+        vendor: VENDOR_LIBS
+    },
+    devtool: 'inline-source-map',
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, './src/index.html')
+        }),
+        new webpack.NamedModulesPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: "vendor",
+            minChunks: function (module) {
+                return module.context && module.context.includes("node_modules");
+            }
+        }),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: "manifest",
+            minChunks: Infinity
+        }),
+    ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    devServer: {
+        port: 8080,
+        contentBase: './src',
+        watchContentBase: true
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: "babel-loader"
+            }
+        }, {
+            test: /\.html$/,
+            use: [{
+                loader: "html-loader",
+                options: {
+                    minimize: true
+                }
+            }]
+        }, {
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader']
+        }, {
+            test: /\.(jpe?g|png|gif|svg)$/,
+            use: ['image-webpack-loader', {
+                loader: 'url-loader',
+                options: {
+                    limit: 40000,
+                    fallback: 'file-loader',
+                    name: 'img/[hash].[ext]'
+                }
+            }]
+        }]
+    }
+};
+
+module.exports = config;
